5a8fbd80b948421ad528a8a2dc01bcf3d2969fed
sigzugern <skyruden@gmail.com>
2022-03-16 16:20:48 +0100

    added a main function receive() for accepting connecting clients and asking for nickname as well as setting up threading.

--- server.py
+++ server.py
@@ -29,3 +29,26 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
+def receive():
+    while True:
+        # Connected user gets accepted and address is sent to server terminal
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        # Asking new client for nickname
+        client.send('NICK'.encode('ascii'))
+        nicknames = client.recv(1024).decode('ascii')
+        clients.append(client)
+
+        # Send messages to users of network of new user
+        print(f"Nickname of the client is {nickname}!")
+        broadcast(f"{nicknames} joined the chat!".encode('ascii'))
+        client.send("Connected to the server!".encode('ascii'))
+
+        # Creating thread so users can be handles "simultaneously"
+        thread = threading.Thread(target=handle(), args=(client,))
+        thread.start()
+
+# Running function
+print("Server is listening...")
+receive()
