d439a3f85f9f6f05019df9ae3123c98ee05c3ab9
sigzugern <skyruden@gmail.com>
2022-03-16 16:04:08 +0100

    Initilising files for assignment, server.py. Adding varibles for later use

---
+++ server.py
@@ -0,0 +1,17 @@
+import threading
+import socket
+
+host = '127.0.0.1'  # localhost
+port = 8181
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
