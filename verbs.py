dfffb4824b2e1b15d82b79e84fd96a8f71afc4d6
Aws  Jaber <52503865+Awsnaser@users.noreply.github.com>
2022-03-28 01:19:19 +0200

    Update verbs.py

--- verbs.py
+++ verbs.py
@@ -1,5 +1,7 @@
 verbs = ["sing"]
-
+# verbs  = ["sing","dance","carol"] ect
+#badverbs  = [" "," ","  "]  you should think about it
+#Hi works = ["hi","hej","hello",Hei] 
 message = "Why don't we call?"
 
 for v in verbs:
